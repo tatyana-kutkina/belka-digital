68c227c3c16748e3ff54859e4d3f213bb035cf6c
Tatyanna-Kutkina <tatyanakutkina.tk@gmail.com>
2023-08-26 14:00:00 +0300

    upload data to clickhouse database

--- parser.py
+++ parser.py
@@ -2,9 +2,13 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 from typing import Optional, Union
+import numpy as np
 
 import re
+from clickhouse_driver import Client
 
+
+# TODO: add check on accurate links in init
 
 class Parser:
     """Class for parsing site 'citystar'."""
@@ -26,7 +30,17 @@ class Parser:
 
     }
 
-
+    def __init__(self, links=None, ):
+        if links is None:
+            self.links = [
+                "http://citystar.ru/detal.htm?sT=1&v_id=1&d=43&ag=&nm=%CE%E1%FA%FF%E2%EB%E5%ED%E8%FF+%2D+%CF%F0%EE%E4%E0%EC+%EA%E2%E0%F0%F2%E8%F0%F3+%E2+%E3%2E+%CC%E0%E3%ED%E8%F2%EE%E3%EE%F0%F1%EA%E5&pS=100",
+                "http://citystar.ru/detal.htm?sT=1&v_id=1&d=43&ag=&nm=%CE%E1%FA%FF%E2%EB%E5%ED%E8%FF+%2D+%CF%F0%EE%E4%E0%EC+%EA%E2%E0%F0%F2%E8%F0%F3+%E2+%E3%2E+%CC%E0%E3%ED%E8%F2%EE%E3%EE%F0%F1%EA%E5&pS=100&pN=2",
+                "http://citystar.ru/detal.htm?sT=1&v_id=1&d=43&ag=&nm=%CE%E1%FA%FF%E2%EB%E5%ED%E8%FF+%2D+%CF%F0%EE%E4%E0%EC+%EA%E2%E0%F0%F2%E8%F0%F3+%E2+%E3%2E+%CC%E0%E3%ED%E8%F2%EE%E3%EE%F0%F1%EA%E5&pS=100&pN=3",
+                "http://citystar.ru/detal.htm?sT=1&v_id=1&d=43&ag=&nm=%CE%E1%FA%FF%E2%EB%E5%ED%E8%FF+%2D+%CF%F0%EE%E4%E0%EC+%EA%E2%E0%F0%F2%E8%F0%F3+%E2+%E3%2E+%CC%E0%E3%ED%E8%F2%EE%E3%EE%F0%F1%EA%E5&pS=100&pN=4",
+                "http://citystar.ru/detal.htm?sT=1&v_id=1&d=43&ag=&nm=%CE%E1%FA%FF%E2%EB%E5%ED%E8%FF+%2D+%CF%F0%EE%E4%E0%EC+%EA%E2%E0%F0%F2%E8%F0%F3+%E2+%E3%2E+%CC%E0%E3%ED%E8%F2%EE%E3%EE%F0%F1%EA%E5&pS=100&pN=5"
+            ]
+        else:
+            self.links = links
 
     def find_room_count(self, text: str) -> Optional[int]:
         """
@@ -70,6 +84,18 @@ class Parser:
         area = float(area_match.group(1)) if area_match else None
         return area
 
+    def find_live_area(self, text: str) -> Optional[float]:
+        """Get live area of the apartment."""
+        area_match = re.search(r'жилая площадь - (\d+\.\d+) кв\.м\.', text)
+        area = float(area_match.group(1)) if area_match else None
+        return area
+
+    def find_kitchen_area(self, text: str) -> Optional[float]:
+        """Get area of the kitchen."""
+        area_match = re.search(r'кухня - (\d+\.\d+) кв\.м\.', text)
+        area = float(area_match.group(1)) if area_match else None
+        return area
+
     def find_floor(self, text: str) -> Union[tuple[int, int], tuple[None, None]]:
         """
         Get total number of floors in the house
@@ -90,33 +116,42 @@ class Parser:
         price = int(price_match.group(1)) if price_match else None
         return price
 
-    def parse(self) -> pd.DataFrame:
-        """Parse apartment information"""
-        res = []
-        for n in ['', 1, 2, 3, 4, 5]:
-            df = pd.DataFrame()
-            URL = f"http://citystar.ru/detal.htm?v_id=1&d=43&nm=%CE%E1%FA%FF%E2%EB%E5%ED%E8%FF+%2D+%CF%F0%EE%E4%E0%EC+%EA%E2%E0%F0%F2%E8%F0%F3+%E2+%E3%2E+%CC%E0%E3%ED%E8%F2%EE%E3%EE%F0%F1%EA%E5{n}"
+    def parse(self, client: Client, table_name: str):
+        """Parse apartment information and add it to clickhouse database"""
+        k = 0
+        for URL in self.links:
             response = requests.get(URL)
-            response.encoding = "cp1251"
-            soup = bs(response.text, "html.parser")
-            descriptions = soup.find_all("tr", class_=["tbb"])
+            if response.status_code == 200:
+                response.encoding = "cp1251"
+                soup = bs(response.text, "html.parser")
+                descriptions = soup.find_all("tr", class_=["tbb"])
 
-            for i, description in enumerate(descriptions):
-                if i == 59 or i == 63:
-                    print(description.text)
-                room_count = self.find_room_count(description.text)
-                district = self.find_district(description.text)
-                floor, total_floors = self.find_floor(description.text)
-                total_area = self.find_total_area(description.text)
-                price = self.find_price(description.text)
-                new_row = {"room_count": room_count, "floor": floor, "total_floors": total_floors, "price": price, "area": total_area,
-                           "district": district,
-                           "description": description.text}
-                df = pd.concat([df, pd.DataFrame(new_row, index=[0])], ignore_index=True)
+                for i, description in enumerate(descriptions):
+                    room_count = self.find_room_count(description.text)
+                    district = self.find_district(description.text)
+                    floor, total_floors = self.find_floor(description.text)
+                    total_area = self.find_total_area(description.text)
+                    live_area = self.find_live_area(description.text)
+                    kitchen_area = self.find_kitchen_area(description.text)
+                    price = self.find_price(description.text)
+                    new_row = {"id": k, "room_count": room_count, "floor": floor, "total_floors": total_floors,
+                               "price": price,
+                               "total_area": total_area,
+                               "live_area": live_area,
+                               "kitchen_area": kitchen_area,
+                               "district": district,
+                               "description": description.text.replace("'", "")}
 
-        return df
+                    values = [str(value) if pd.notna(value) else np.nan for value in new_row.values()]
+                    values = [f"'{value}'" if isinstance(value, str) else str(value) for value in values]
+                    print(values)
+                    query = f"INSERT INTO {table_name} ({', '.join(new_row.keys())}) VALUES ({', '.join(values)})"
+                    client.execute(query)
+                    k += 1
+
 
 if __name__ == "__main__":
     parser = Parser()
-    df = parser.parse()
-    df.to_csv("parsed_data.csv")
+    client = Client(host='localhost', port='9000', settings={'use_numpy': True})
+    table_name = "belka_digital.apartment_table"
+    parser.parse(client=client, table_name=table_name)
